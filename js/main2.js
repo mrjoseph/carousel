0ff8802fc8f26bc92ee70b0d698fcaf2587d58e6
mrjoseph <trev_jos@hotmail.com>
2012-12-06 07:57:35 +0000

    Reworked! Carousel is now responsive. Having a problem with the page count. Need to re think this.

--- js/main2.js
+++ js/main2.js
@@ -1,125 +1,99 @@
 var carousel = carousel || {};
 carousel = {
 	carouselFrame		: $('#carousel-container'),
-	li					: $('.carousel-container li'),
+	li					: $('#carousel-container li'),
 	outsideContainer	: $('#outside-container'),
-	curScrollAmount		: null,
-	liNum				: null,
-
+	liNum				: $('#carousel-container li').length,
+	results				: [],
 	resize : function(){
 		var
-		results               = [];
-		liNum                 = carousel.li.length,
 		containerWidth        = carousel.outsideContainer.width(),
-		carousel.curScrollAmount  = containerWidth,
-		maxWidth              = liNum * containerWidth,
+		maxWidth              = carousel.liNum * containerWidth*2,
 		maxLength             = maxWidth - containerWidth,
 		carouselWidth         = carousel.carouselFrame.width(maxWidth);
-		carouselMarginWidth   = parseInt(carousel.carouselFrame.css('margin-left').replace('px',''),'');
 		carousel.li.width(containerWidth);
-		results.push(containerWidth,maxWidth,maxLength);
-		console.log('new margin: ',carouselMarginWidth);
-		console.log('new margin: ',carouselMarginWidth+=1);
-		//carousel.carouselFrame.animate({marginLeft : carouselMarginWidth+=1});
-		return results;
+		carousel.results.push(containerWidth,maxWidth,maxLength);
 	},
 	nav : function(){
 		var
 		moveLeft	= '+=',
 		moveRight	= '-=';
 		$('a').click(function(e){
-			var NewPageCount = new	carousel.pageCount();	
+			if(carousel.initNo  === carousel.liNum){ // reset it to the total number of lis
+				$('.counter .countNo').html('1');
+			}
+			
 			moveDistance = $('#carousel-container li').width();
 			e.preventDefault();
 			var direction = $(this).attr('class');
 			distance = parseInt($('#carousel-container').css('margin-left').replace('px',''),'');
-			var limit = new carousel.resize();
 
 			if( direction ==='right' ){
-				if( distance <= -limit[2] ){
-					return false;
-				}
-				carousel.move(moveRight,moveDistance);
-				NewPageCount.addNo();
-				$('.number').html(carousel.initNo);
+				carousel.move(moveRight,moveDistance,direction);
 			} else
+
 			if( direction === 'left' ){
-				if( distance >= 0 ){
-					return false;
-				}
-				carousel.move(moveLeft,moveDistance);
-				NewPageCount.deleteNo();
-				$('.number').html(carousel.initNo);
+				carousel.move(moveLeft,moveDistance,direction);	
 			}
 			carousel.busy = true;
 		});
 	},
 	busy : false,
-	move:function(dir,moveDistance){
+	move:function(dir,moveDistance,direction){
+		var NewPageCount = new	carousel.pageCount();	
 		if( !carousel.busy ){ 
-			$('#carousel-container').stop().animate({marginLeft : dir+moveDistance},500,function(){
-				carousel.busy = false;
-			});
+			 if( direction ==='right' ){
+				$('#carousel-container').stop().animate({marginLeft : dir+moveDistance},500,function(){
+					$('#carousel-container li:first').remove().insertAfter($('#carousel-container li:last'));
+					$('#carousel-container').css('margin-left',0);
+					NewPageCount.addNo();
+					$('.counter .countNo').html(carousel.initNo);
+			 		carousel.busy = false;
+			 	});
+			 } else
+			 if( direction ==='left' ){
+				$('#carousel-container li:last').remove().insertBefore($('#carousel-container li:first'));
+				$('#carousel-container').css('margin-left',-moveDistance);
+				$('#carousel-container').stop().animate({marginLeft : dir+moveDistance},500,function(){
+					NewPageCount.deleteNo();
+					$('.counter .countNo').html(carousel.initNo);
+			 		carousel.busy = false;
+			 	});
+			 }
 		}
 	},
 
-	initNo : '1',
+	initNo : 1,
 	pageCount: function(){
 		this.addNo = function(){
-			return carousel.initNo++;
+			this.zeroNo();
+			return carousel.initNo ++;
 		};
+
 		this.deleteNo = function(){
-			return carousel.initNo--;
+			this.zeroNo();
+			return carousel.initNo --;
 		};
-	$('.number').html(carousel.initNo);	
-	},
-	resetCarousel : function(){
-    var
-    doit,
-    startStopWidth = [],
-    reminderGap,
-	carouselWidth = $('#carousel-container').width();
-    startStopWidth.push(carouselWidth);
-    // console.log(startStopWidth);
-    
-    function stopResize(num){
-      var currentMargin = parseInt($('#carousel-container').css('margin-left').replace('px',''),'');
-      var w = $('.carousel-container li').width();
-      startStopWidth.push(w);
-      if(startStopWidth.length > 2){
-        delete startStopWidth[0];
-        startStopWidth.splice(0,1);
-      }
-      reminderGap = startStopWidth[0] - startStopWidth[1];
-      var newMargin = currentMargin - reminderGap;
 
-      var newPosition = currentMargin + reminderGap*num;
-
-      //shit the carousel back into place
-      if(currentMargin !== 0){
-        $('#carousel-container').animate({
-           marginLeft: newPosition+'px'
-        });
-      }
-    }
-    $(window).resize(function(){
-      (function(){
-        clearTimeout(doit);
-        doit = setTimeout(function() {
-			var numInc = carousel.initNo-1;
-			// console.log(numInc, carousel.initNo);
-          stopResize(0);
-        }, 500);
-      }());
-    });
-  }
+		this.zeroNo = function(){	
+			if(carousel.initNo === carousel.liNum){
+				carousel.initNo = 0;			
+			} else
+			if(carousel.initNo === 0){
+				console.log('you are now zero', carousel.liNum);
+				carousel.initNo = carousel.liNum;
+				$('.counter .countNo').html(carousel.liNum);			
+			}
+		};
+	$('.counter .countNo').html(carousel.initNo);	
+	$('.counter .totalNo').html(carousel.liNum);	
+	}
 };
 
 $(document).ready(function(){
 	carousel.resize();
 	carousel.nav();
-	carousel.pageCount();
-	carousel.resetCarousel();
+	carousel.pageCount();	
 });
 $(window).resize(function(){
 	carousel.resize();
